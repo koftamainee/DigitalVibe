3405dc1d4b21b20c098056e594cbc3732851ba77
koftamainee <koftamainee@gmail.com>
2024-10-18 18:13:10 +0300

    update routing

--- backend/src/routes/routes.go
+++ backend/src/routes/routes.go
@@ -8,39 +8,47 @@ import (
 func SetupRoutes(mux *http.ServeMux) {
 	// GET
 	mux.HandleFunc("GET /", rootHandler)
-	mux.HandleFunc("GET /c", allChatsGetHandler)
-	mux.HandleFunc("GET /c/{id}", chatGetHandler)
 
-	// POST
-	mux.HandleFunc("POST /c}", chatPostHandler)
+	mux.HandleFunc("GET /login/", loginHandler)
+
+	mux.HandleFunc("GET /chat/ai", aiChatGetHandler)
+	mux.HandleFunc("GET /chat/psychologist", psychologistChatGetHandler)
+
+	mux.HandleFunc("GET /tracker/", trackersHandler)
+	mux.HandleFunc("GET /tracker/emotion", emotionTrackerHandler)
+	mux.HandleFunc("GET /tracker/stress", stressTrackerHandler)
+	mux.HandleFunc("GET /tracker/diary", diaryTrackerHandler)
+
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Hello, World!</title>
-	</head>
-	<body>
-		<h1>Hello, World!</h1>
-		<p>Welcome to my web server!</p>
-	</body>
-	</html>
-`)
+	fmt.Fprintf(w, "main page")
 }
 
-func allChatsGetHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from all chats")
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "login page")
 }
 
-func chatGetHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
-	fmt.Fprintf(w, "Hello from chat, id = %s", id)
+func aiChatGetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ai chat page")
 }
 
-func chatPostHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "created new chat")
+func psychologistChatGetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "psyhologist chat page")
+}
+
+func trackersHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "All trackers")
+}
+
+func emotionTrackerHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "emotion tracker")
+}
+
+func stressTrackerHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "stress tracker")
+}
+
+func diaryTrackerHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "diary tracker")
 }
