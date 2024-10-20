7d1d8169b379851aa270d8bbe86c84200f75b1d3
koftamainee <koftamainee@gmail.com>
2024-10-21 00:29:17 +0300

    move to nginx

--- task-service/main.go
+++ task-service/main.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 
 	"main/handlers"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
 	handlers.SetupRoutes(mux)
+
+	http.Handle("/metrics", promhttp.Handler())
 
 	log.Println("Task Service running on port 8082")
 	log.Fatal(http.ListenAndServe(":8082", mux))
